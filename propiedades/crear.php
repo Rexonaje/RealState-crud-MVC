0f2add29a9caa47b9cd8708d71a9e68c87bd0ceb
Carlos Nicolas Brambilla <97968900+Rexonaje@users.noreply.github.com>
2024-07-09 13:10:14 -0300

    fixed bug that make url with message useless

---
+++ propiedades/crear.php
@@ -0,0 +1,16 @@
+<main class="contenedor secccion">
+    <h1>crear</h1>
+ 
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+                <?php echo $error;?> 
+            </div>
+        <?php endforeach; ?>   
+      
+    <form method="POST" class="formulario" enctype= "multipart/form-data">
+        <?php  include __DIR__ . '/formulario.php';?>
+        <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+    </form>
+         
+
+</main>
