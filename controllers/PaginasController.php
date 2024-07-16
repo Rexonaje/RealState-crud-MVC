f12f0a2c4e302ffef4325de67b7a172fd16f5af7
Carlos Nicolas Brambilla <97968900+Rexonaje@users.noreply.github.com>
2024-07-16 17:39:06 -0300

    added working header links

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -5,33 +5,39 @@
  class PaginasController{
     public static function index(Router $router){
         $propiedades=Propiedad::get(3);
+        $inicio=true;
       $router->render('paginas/index',[
-        'propiedades'=>$propiedades
+        'propiedades'=>$propiedades,
+        'inicio'=> $inicio
       ]);
     }
-    public static function nosotros(){
-        echo "nosotros"; 
+    public static function nosotros(Router $router){
+                                
+        $router->render('paginas/nosotros',[]);
         
     }
-    public static function propiedades(){
-        echo "propiedades"; 
+    public static function propiedades(Router $router){
+        $propiedades=Propiedad::all();
+        $router->render('paginas/propiedades',['propiedades'=>$propiedades ]);
         
     }
     
-    public static function propiedad(){
-        echo "propiedad"; 
+    public static function propiedad(Router $router){
+        $id= validarORedireccionar('/propiedades');
+        $propiedad=Propiedad::find($id);
+        $router->render('paginas/propiedad',['propiedad'=>$propiedad ]);
         
     }
-    public static function blog(){
+    public static function blog(Router $router){
         
-        echo "blog"; 
+        $router->render('paginas/blog',[]);
     }
-    public static function entrada(){
-        echo "entrada"; 
+    public static function entrada(Router $router){
+        $router->render('paginas/entrada',[]);
         
     }
-    public static function contacto(){
-        echo "contacto"; 
+    public static function contacto(Router $router){
+        $router->render('paginas/contacto',[]);                             
         
     }
  }
