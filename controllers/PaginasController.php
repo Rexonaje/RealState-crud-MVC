3495b7983c608116e00c2ece1f3f2dc14c146c60
Carlos Nicolas Brambilla <97968900+Rexonaje@users.noreply.github.com>
2024-07-23 14:11:53 -0300

    created configuration fort phpMailer

--- controllers/PaginasController.php
+++ controllers/PaginasController.php
@@ -2,6 +2,8 @@
  namespace Controller;
  use MVC\Router;
  use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
+
  class PaginasController{
     public static function index(Router $router){
         $propiedades=Propiedad::get(3);
@@ -37,6 +39,37 @@
         
     }
     public static function contacto(Router $router){
+        if($_SERVER['REQUEST_METHOD']==='POST'){
+            //crear una instancia de phpmailer
+            $mail=new PHPMailer();
+            //Configurar smtp
+            $mail->isSMTP();
+            $mail->Host='smtp.mailtrap.io';
+            $mail->SMTPAuth=true;
+            $mail->Username='c20fc8989472f9';
+            $mail->Password='07754e70f5d1b0';
+            $mail->SMTPSecure='tls';//No encriptados pero por un canal seguro
+            $mail->Port=2525;
+            //configurar el contenido del mail
+            $mail->setFrom('admin@bienesraices.com');// quien envia
+            $mail->addAddress('admin@bienesraices.com','BienesRaices.com');//quien recibe
+            $mail->Subject='Tienes un nuevo mensaje';
+
+            //habilitar html
+            $mail->isHTML(true);
+            $mail->CharSet='UTF-8';
+            //definir contenido
+            $contenido='<html> <p> Tienes un nuevo mensaje </p> </html> ';
+            $mail->Body=$contenido;
+            $mail->AltBody='Tienes un nuevo mensaje';
+            //enviar mail
+            if($mail->send()){
+                echo "Mensaje enviado correctamente";
+            }
+            else{
+                echo "error al enviar mensaje";
+            }
+        }
         $router->render('paginas/contacto',[]);                             
         
     }
